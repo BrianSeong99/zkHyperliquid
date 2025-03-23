c232e418edb3e27c10a6c12e08db3289293e424c
Brian Seong <BrianSeong.dev@gmail.com>
2025-03-23 07:58:09 -0700

    feat: added zkVM program for block execution proof

---
+++ zkVM/lib/src/data.rs
@@ -0,0 +1,101 @@
+// prepare data for zkVM
+
+use crate::types::*;
+use rand;
+use std::collections::{HashMap, VecDeque};
+use std::time::{SystemTime, UNIX_EPOCH};
+use std::fmt::Write;
+
+pub fn populate_block_data(num_orders: usize) -> BlockProofInput {
+    // Generate all orders first
+    let mut orders = Vec::with_capacity(num_orders);
+    for i in 0..num_orders {
+        orders.push(Order::new(
+            i.to_string(),
+            format!("user{}", (i % 3) + 1),
+            "POL-ETH".to_string(),
+            50 + rand::random::<u64>() % 51, // 50-100
+            95 + rand::random::<u64>() % 11, // 95-105
+            true
+        ));
+    }
+    let mut matched_logs = HashMap::new(); // HashMap<String, VecDeque<MatchedEntry>>
+    for order in orders {
+        matched_logs.entry(order.pair_id.clone()).or_insert(VecDeque::new()).push_back(MatchedEntry::new(order));
+    }
+    let block1 = Block {
+        id: "1".to_string(),
+        last_block_hash: [0u8; 32],
+        timestamp: SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_secs(),
+        length: matched_logs.len() as u64,
+        logs: matched_logs,
+    };
+    let mut block2 = block1.clone();
+    let block1_hash = block1.hash();
+    block2.last_block_hash = block1_hash.clone();
+    let block2_hash = block2.hash();
+
+    let mut previous_users_balance_state = UserBalanceState { users: HashMap::new() };
+    let mut users_balance_state = UserBalanceState { users: HashMap::new() };
+
+    let mut user1 = User {
+        address: "user1".to_string(),
+        balances: 1000000000,
+    };
+    let mut user2 = user1.clone();
+    user2.address = "user2".to_string();
+    let mut user3 = user1.clone();
+    user3.address = "user3".to_string();    
+
+    // create user balance state
+    previous_users_balance_state.users.insert("user1".to_string(), user1.clone());
+    previous_users_balance_state.users.insert("user2".to_string(), user2.clone());
+    previous_users_balance_state.users.insert("user3".to_string(), user3.clone());
+
+    // now calculate the users new balance state after the block is settled
+    for pair in block1.logs.values() {
+        for matched_entry in pair {
+            if matched_entry.buy_order.user_id == "user1" {
+                user1.balances += matched_entry.matched_amount;
+            }
+            if matched_entry.buy_order.user_id == "user2" {
+                user2.balances += matched_entry.matched_amount;
+            }
+            if matched_entry.buy_order.user_id == "user3" {
+                user3.balances += matched_entry.matched_amount;
+            }
+            if matched_entry.sell_order.user_id == "user1" {
+                user1.balances -= matched_entry.matched_amount;
+            }
+            if matched_entry.sell_order.user_id == "user2" {
+                user2.balances -= matched_entry.matched_amount;
+            }   
+            if matched_entry.sell_order.user_id == "user3" {
+                user3.balances -= matched_entry.matched_amount;
+            }
+        }
+    }
+
+    users_balance_state.users.insert("user1".to_string(), user1.clone());
+    users_balance_state.users.insert("user2".to_string(), user2.clone());
+    users_balance_state.users.insert("user3".to_string(), user3.clone());
+
+    let user_balance_hash = users_balance_state.hash();
+    let previous_user_balance_hash = previous_users_balance_state.hash();
+
+    let block_proof_input = BlockProofInput {
+        block: block1,
+        hash: block1_hash,
+        user_balance_hash: user_balance_hash,
+        previous_user_balance_hash: previous_user_balance_hash,
+        previous_user_balance_state: previous_users_balance_state,
+        user_balance_state: users_balance_state,
+    };
+
+    println!("Data: Expected user balance hash: {:?}", user_balance_hash);
+
+    block_proof_input
+}
---
+++ zkVM/lib/src/types.rs
@@ -0,0 +1,183 @@
+use alloy_sol_types::sol;
+use serde::{Serialize, Deserialize};
+use std::cmp::Ordering;
+use std::collections::{HashMap, VecDeque};
+use std::time::{SystemTime, UNIX_EPOCH};
+use std::hash::{Hash, Hasher};
+use std::collections::hash_map::DefaultHasher;
+use serde_json;
+
+
+// Define bytes32 type (typically a 32-byte array)
+pub type bytes32 = [u8; 32];
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct Order {
+    pub id: String,
+    pub user_id: String,
+    pub pair_id: String,
+    pub amount: u64,
+    pub filled_amount: u64,
+    pub price: u64,
+    pub side: bool,
+    pub status: OrderStatus,
+    pub created_at: u64,
+    pub updated_at: u64,
+}
+
+impl Order {
+    // Create a new order with a given id, user_id, pair_id, amount, price, and side
+    // The status is pending by default, 
+    // then once the order is matched, the status is matched, 
+    // then when block is settled on L1, then the status is settled
+    pub fn new(
+        id: String, 
+        user_id: String, 
+        pair_id: String, 
+        amount: u64, 
+        price: u64, 
+        side: bool
+    ) -> Self {
+        let now = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_secs();
+        
+        Self { 
+            id, user_id, pair_id, amount,
+            filled_amount: 0,
+            price, side,
+            status: OrderStatus::Pending, 
+            created_at: now,
+            updated_at: now
+        }
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+pub enum OrderStatus {
+    Pending, // pending to be filled
+    PartiallyFilled, // partially filled
+    Filled, // fully filled
+    Cancelled, // cancelled
+    Batched, // batched to by epoch block builder
+    Settled, // settled on L1
+}
+
+pub struct BlockProof {
+    length: u32,
+    hash: bytes32,
+    previous_hash: bytes32,
+    user_balance_hash: bytes32,
+    previous_user_balance_hash: bytes32,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct MatchedEntry {
+    pub timestamp: u64, 
+    pub buy_order: Order, 
+    pub sell_order: Order, 
+    pub matched_amount: u64
+}
+
+impl PartialEq for MatchedEntry {
+    fn eq(&self, other: &Self) -> bool {
+        self.timestamp == other.timestamp
+    }
+}
+
+impl Eq for MatchedEntry {}
+
+impl PartialOrd for MatchedEntry {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl MatchedEntry {
+    pub fn new(buy_order: Order) -> Self {
+        Self {
+            timestamp: SystemTime::now()
+                .duration_since(UNIX_EPOCH)
+                .unwrap()
+                .as_secs(),
+            buy_order: buy_order.clone(),
+            sell_order: Order::new(buy_order.id, buy_order.user_id, buy_order.pair_id, buy_order.amount, buy_order.price, !buy_order.side),
+            matched_amount: buy_order.amount
+        }
+    }
+}
+
+impl Ord for MatchedEntry {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.timestamp.cmp(&other.timestamp)
+    }
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct UserBalanceState {
+    pub users: HashMap<String, User>, // user id -> user
+}
+
+impl UserBalanceState {
+    pub fn hash(&self) -> bytes32 {
+        let mut hasher = DefaultHasher::new();
+        // json serialize the block
+        let user_balance_state_json = serde_json::to_string(self).unwrap();
+        hasher.write(user_balance_state_json.as_bytes());
+        let hash_value = hasher.finish();
+        
+        // Create a 32-byte array filled with zeros
+        let mut result = [0u8; 32];
+        
+        // Copy the 8 bytes from hash_value into the last 8 bytes of result
+        result[24..32].copy_from_slice(&hash_value.to_le_bytes());
+        
+        result
+    }
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct User {
+    pub address: String,
+    pub balances: u64, // using u64 for simplicity, can be extended to HashMap<String, u64>
+}
+
+// Block datatype
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct Block {
+    pub id: String,
+    pub last_block_hash: bytes32,
+    pub timestamp: u64,
+    pub length: u64,
+    pub logs: HashMap<String, VecDeque<MatchedEntry>>,
+    // TODO: add balance merkle tree root hash, last block BMR Hash, and related fields.
+}
+
+impl Block {
+    pub fn hash(&self) -> bytes32 {
+        let mut hasher = DefaultHasher::new();
+        // json serialize the block
+        let block_json = serde_json::to_string(self).unwrap();
+        hasher.write(block_json.as_bytes());
+        let hash_value = hasher.finish();
+        
+        // Create a 32-byte array filled with zeros
+        let mut result = [0u8; 32];
+        
+        // Copy the 8 bytes from hash_value into the last 8 bytes of result
+        result[24..32].copy_from_slice(&hash_value.to_le_bytes());
+        
+        result
+    }
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct BlockProofInput {
+    pub block: Block,
+    pub hash: bytes32,
+    pub user_balance_hash: bytes32,
+    pub previous_user_balance_hash: bytes32,
+    pub previous_user_balance_state: UserBalanceState,
+    pub user_balance_state: UserBalanceState,
+}
